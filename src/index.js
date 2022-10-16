141e208e6834aeb88ecf71282b7a0b3c5b9e26f2
Madhukesh K T <madhukeshthakur@gmail.com>
2022-10-16 17:02:06 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dbConnect = require("./config/db");
 const userRouter = require("./models/user.router");
 require("dotenv").config();
@@ -7,8 +8,10 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 
+app.use(cors());
 app.use("/users", userRouter);
 app.listen(PORT, async () => {
   await dbConnect();
